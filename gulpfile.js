280811dfaf29956bd11c887b25d7359f924a11dc
kumar <eahemak@hotmail.com>
2017-12-18 18:59:51 +0100

    added app.js in source folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,20 @@
 // grab our gulp packages
 var gulp  = require('gulp'),
 gutil = require('gulp-util');
+jshint     = require('gulp-jshint'),
 
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
+
+// configure the jshint task
+gulp.task('jshint', function() {
+    return gulp.src('source/**/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'));
+  });
+  
 // create a default task and just log a message
-gulp.task('default', function() {
-return gutil.log('Gulp is running!')
-});
+gulp.task('watch', function() {
+    gulp.watch('source/**/*.js', ['jshint']);
+  });
+  
